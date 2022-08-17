6265c74ad1d68ee165d4ce4c2e56708ca03bdee1
utsho-gif <iftakharifti@hotmail.com>
2022-08-17 15:22:22 +0600

    deploying to firebase

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,7 +1,6 @@
 import chair from "../../assets/images/chair.png";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
-import { format } from "date-fns";
 
 const AppointmentBanner = ({ date, setDate }) => {
   return (
